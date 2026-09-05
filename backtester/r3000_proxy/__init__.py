d4c96bc6a9e3808020dbb9e204bd677486bdce67
flabber1835 <m.bron01@gmail.com>
2026-09-04 17:36:54 -0700

    Add R3000 proxy corpus package

---
+++ backtester/r3000_proxy/__init__.py
@@ -0,0 +1,4 @@
+"""PIT Russell 3000 proxy corpus derived from contemporaneous IWB/IWM holdings."""
+
+CORPUS_ID = "r3000-proxy-pit-2006-2026-v1"
+PROXY_CAVEAT = "This is an IWB/IWM-derived Russell 3000 proxy and is not a licensed FTSE Russell constituent history."
